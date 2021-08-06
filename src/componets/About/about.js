58851f194082b16acaf4c882407a3c757eb3d365
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-08-06 00:04:20 -0700

    added bootstrap to about cards

--- src/componets/About/about.js
+++ src/componets/About/about.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
 function About(props){
 
   return(
@@ -9,18 +12,25 @@ function About(props){
         <p> These are the people who are are behind Ravenmoore Valley and the marketplace.</p>
       </article>
       <section class="about-cards">
-        <div class="creator">
-          <h3 class="title">Mastermind</h3>
-          <h3 class="name">Muninn (Matt) Ravenmoore</h3>
-          <p class="bio">The visonary behind ravenmoore valley and Head of Clan Ravenmoore</p>
-        </div>
-        <div class="creator">
-          <h3 class="title">Marketing PR and Social Media</h3>
-          <h3 class="name">Huginn (Rebecca) Ravenmoore</h3>
-          <p class="bio"> Huginn handles alot of the being on the Social medias and such (FILLER TEXT FILLER)</p>
-        </div>
+        <Card style={{width: '360px'}}>
+          <Card.Img variant="left" src="../../public/images/thing.jpg" alt="Headshot" />
+          <Card.Body>
+            <Card.Title>Muninn (Matt) Ravenmoore</Card.Title>
+            <Card.Text>About The MasterMind</Card.Text>
+          </Card.Body>
+          <Button variant= "dark">Portfolio</Button>
+        </Card>
+
+        <Card style={{width: '360px'}}>
+          <Card.Img variant="left" src="../../public/images/thing.jpg" alt="Headshot" />
+          <Card.Body>
+            <Card.Title>Huginn (Rebecca) Ravenmoore</Card.Title>
+            <Card.Text>About Social enginer and Media Relations</Card.Text>
+          </Card.Body>
+          <Button variant= "dark">Portfolio</Button>
+        </Card>
       </section>
-    </div>
+      </div>  
   )
 }
 
