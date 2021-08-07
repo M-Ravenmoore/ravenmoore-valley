52185e4ac3d0aff49ad16da4b97259af94fc9908
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-08-06 17:24:49 -0700

    about cards

--- src/componets/Home/home.js
+++ src/componets/Home/home.js
@@ -5,9 +5,22 @@ function Home(props){
   return(
     <div id="appHome" className="Home">
       <section id="first-row">
-        <h2> welcome to the Ravenmoore Valley Marketplace</h2>
+        <h2> Welcome to the Ravenmoore Valley Marketplace</h2>
 
-        <p> this will be the home page with several rows of info and links</p>
+        <p> Here at Ravenmoore Valley Marketplace, we bring together artists, crafters, and makers with creative and unique skills.</p>
+      </section>
+
+      <section id="second-row">
+        {/* want an img here either as background or in the row */}
+        <h2>Custom Works</h2>
+
+        <p> Many of our artisans are available for Commission and other custom works. See the Ravenmoore Commission gallery or our Commissioners list for more details.</p>
+      </section>
+
+      <section id="third-row">
+        <h2>The Goods</h2>
+{/* add link to Store here */}
+        <p> Soon we will have our own store, one day when we grow up, but for now you can click below and checkout our current third party store for the Ravenmoore Clan. </p>
       </section>
     </div>  )
 };
