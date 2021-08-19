e1fca36f0d71496067e2c479ea28ed85acd706c5
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-08-18 20:54:54 -0700

    changes and tweeks to content

--- src/componets/Store/store-temp.js
+++ src/componets/Store/store-temp.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 function Store (props){
 
   return(
     <div id='store'>
     <h2>Store</h2>
+    <Button href="https://www.etsy.com/shop/RavenmooreValleyShop?ref=l2-about-shopname">Etsy Store</Button>
     </div>
   )
 }
