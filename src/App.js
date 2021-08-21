432a82bb55b5e43707f6614c6e508e9e6aa57426
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-08-20 17:38:22 -0700

    styles are started

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Main from './componets/Main/main'
 import NavBar from './componets/Nav-Bar/nav'
 import Footer from './componets/Footer/footer';
 import './reset.css';
-<<<<<<< HEAD
 import './Styles/app-dark.css';
-=======
-import './App.css';
->>>>>>> 80b93afa81526076d90a5eab0f087b2142cb8c3f
 
 function App() {
   return (
