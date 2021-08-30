52051b65fdfb46722f50ce3fb5be4956027ce551
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-08-30 12:43:21 -0700

    removed unused code

--- src/componets/Workshop/Projects/Arcade/arcade.js
+++ src/componets/Workshop/Projects/Arcade/arcade.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ArcadeSlides from './slideshow';
 
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 
 function Arcade (props){
 
