d2625abc4a9970159aeeb5f116a3aa65645cdbec
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-09-04 13:13:09 -0700

    nav bar response fix.

--- src/componets/Nav-Bar/nav.js
+++ src/componets/Nav-Bar/nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Navbar } from "react-bootstrap";
+import { Navbar, NavDropdown } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 
@@ -12,7 +12,7 @@ function NavigationBar(props){
   return(
 
     <>
-  <Navbar variant="dark" expand="lg">
+  <Navbar variant="dark" expand="md" sticky="top">
     <Container>
     <Navbar.Brand href="#/">
       <img
@@ -23,16 +23,21 @@ function NavigationBar(props){
         alt="Ravenmoore logo"
       />
     </Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#/">Home</Nav.Link>
-      <Nav.Link href="#about">About</Nav.Link>
-      <Nav.Link href="#artists">Artisans</Nav.Link>
-      <Nav.Link href="#news">News</Nav.Link>
-      <Nav.Link href="#workshop">Workshop</Nav.Link>
-      <Nav.Link href="#menagerie">Menagerie</Nav.Link>
-      <Nav.Link href="#store">Store</Nav.Link>
-      <ThemeSwitch/>
-    </Nav>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#/">Home</Nav.Link>
+        <Nav.Link href="#about">About</Nav.Link>
+        <Nav.Link href="#artists">Artisans</Nav.Link>
+        <Nav.Link href="#news">News</Nav.Link>
+        <NavDropdown title="Rooms" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#workshop">Workshop</NavDropdown.Item>
+          <NavDropdown.Item href="#menagerie">Menagerie</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#store">Store</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>    
+    <ThemeSwitch/>
     </Container>
   </Navbar>
   
