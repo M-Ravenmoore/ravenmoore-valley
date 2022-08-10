29eac6a9f658a333baf18ac6867ceb1955c98ade
Matt Ravenmoore <m.ravenmoore@gmail.com>
2022-08-09 23:30:36 -0700

    about uptodate

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -10,47 +10,34 @@ function About(props){
     <div id="appAbout" className="About">
       <article className="content-A">
         <h2>About The Ravenmoore Community</h2>
-        <p> Ravenmoore Valley, is the community built by Muninn (Matt) and Huginn (Rebecca) Ravenmoore and their close friends. It is slowly growing, and we hope it continues. We are Artists creators, designers, builders and more. This community is about creating and thriveing together as a group.  As you browse our community site you will find we do many things and involve our comunity in most of it.  From renactment events, to local and streamed bonfires, charity events / drives, Twitch streaming, creating, community gardening, even a store to sell our creations to our lovely community members. </p>
+        <p> Ravenmoore Valley, is more than just a store or a few artists it is a community, Driven by its founder Dusk (Matt) Ravenmoore, but shaped and added to but everyone involved from the artists the to our amazing clients that come to us asking to make their dreams and wishes come true, to viwers that watch from afar via Insta and Twitch alike.</p>
+        <p>Our Goal is to bring to gether small crafters and creators that art too small to make market on their own and give them a startig place to sell thier wares... Given they fit our Renasuance motive and a few other guidelines.... </p>
       </article>
 
         <h3>The Community Leaders and Rolls</h3>
       <section className="founder-cards">
       
         <Card className="founders">
-          <Card.Img className='Headshot' src="./images/Headshots/matt-headshot.png" alt="Muninn Ravenmoore Headshot" />
+          <Card.Img className='Headshot' src="./images/Headshots/matt-headshot.png" alt="Dusk Ravenmoore Headshot" />
           <Card.Body>
-            <Card.Title>Mastermind</Card.Title>
-            <Card.Subtitle>Muninn (Matt) Ravenmoore</Card.Subtitle>
-            <Card.Text> Meet Muninn Ravenmoore. He is The head of the Ravenmoore Family and The visonary, and driving force behind The Ravenmoore Valley Community. Muninn is primarily a metal artist and but he dabbles is many others, a true "jack of all trades master of few" kind of guy.  He  regularly 'collects' artists and crafters that have talent but no ability to make a booth or shop into The Ravenmoore Valley Market.</Card.Text>
+            <Card.Title>Founder</Card.Title>
+            <Card.Subtitle>Dusk (Matt) Ravenmoore</Card.Subtitle>
+            <Card.Text> Meet Dusk Ravenmoore. He is The head of House Ravenmoore. The visonary, and driving force behind Ravenmoore Valley and its Community. Dusk is primarily a metal artist and can work with most anything else excepting glass.  He  regularly 'collects' artists and crafters that have talent but no ability to make a booth or shop into The Ravenmoore Valley Market. His shop withing ravenmoore valley is The seared scale click below to wander in</Card.Text>
           </Card.Body>
           <div className="cardBtn">
-          <Button variant="dark" href='#workshop' >Workshop</Button>
+          <Button variant="dark" href='#seardScale' >The Seared Scale</Button>
           </div>
         </Card>
 
         <Card className="founders">
-          <Card.Img className='Headshot' src="./images/Headshots/Huginn-Headshot.jpg" alt="Headshot" />
+          <Card.Img className='Headshot' src="./images/Headshots/Dawn-Headshot.jpg" alt="Dawn-Headshot" />
           <Card.Body>
-            <Card.Title>Marketing PR and Social Media</Card.Title>
-            <Card.Subtitle>Huginn (Rebecca) Ravenmoore</Card.Subtitle>
-            <Card.Text> Meet Huginn Ravenmoore, wife to Muninn, and often the public face of Ravenmoore Valley. Her crafts include Crocheting, sewing, papercraft and more. </Card.Text>
+            <Card.Title>Co-Owner</Card.Title>
+            <Card.Subtitle>Dawn Gallows</Card.Subtitle>
+            <Card.Text> Dawn will be writing her own bio here shortly folks just you wait her botainicals do wonderous things! </Card.Text>
           </Card.Body>
           <div className="cardBtn">
-           <Button href='#menagerie' variant= "dark">Menagerie</Button>
-          </div>
-        </Card>
-      
-
-        <Card className="founders">
-          <Card.Img className='Headshot' src="./images/HomeSlides/saw-heart.jpg" alt="Headshot" />
-          <Card.Body>
-            <Card.Title>The Ravenmoore Artisans</Card.Title>
-            <Card.Subtitle>The Artists, Crafters, ect</Card.Subtitle>
-            <Card.Text> These are the people that have asked for space in our Ravenmoore Valley Market. The Creators, Makers, Tinkers, and more. Usualy small little know artists that need a place to seell their wears. you can find out more by clicking below</Card.Text>
-          </Card.Body>
-          <div className="cardBtn">
-            <Button href="#" variant="dark">Market Application</Button>
-            <Button href='#Artists' variant="dark">Artists</Button>
+           <Button href='#flameingFeather' variant= "dark">Flameing Feather</Button>
           </div>
         </Card>
       </section>
