542d90411f0607bc796bd2db86f6c2341825bfbb
Matt Ravenmoore <m.ravenmoore@gmail.com>
2022-11-20 19:35:07 -0800

    home page updated with content"

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -17,20 +17,20 @@ function Home(props){
       </section>
 
       <section className="content-A" id="second-row">
-      <h2>The Ravenmoore Family</h2>
-        <p> The Cornerstone of Ravenmoore Valley is The founder, Matt or Dusk Ravenmoore. He along side Dawn have created the unique space here where many artists can share thier wares with the world. As such each Artisan of Ravenmoore valley has their own Shop within the valley where youll find their fine creations. Head on over to the Artisans Directory to browse by artist. </p>
+      <h2>Explore The Valley!</h2>
+        <p> Come along and explore ravenmoore valley get to know its people, take a looka a the wares, learn something new perhaps. its all below just a click away.</p>
       </section>
 
       <div className="panels" id="panelContainer">
-        <section className="infoPanel-A" id="Scale">
+        <section className="infoPanel-A" id="about">
           <img 
             className="panelImg"
             src="./images/Logo/SearedScaleLogo.png"
             alt="seared_scale_logo"
           />
-          <h4>The Seared Scale</h4>
-          <p>Custom Crafts and Wonderous Creations are imagined in Dusks Lair Come see!</p>
-          <Button href='#artists' variant='dark'>Artisans directory</Button>
+          <h4>Sit by the fire</h4>
+          <p>Come sit by the fire and hear all about Ravenmoore Valley and how it came to be.</p>
+          <Button href='#about' variant='dark'>About us</Button>
         </section>
 
         <section className="infoPanel-A" id="Feather">
@@ -39,9 +39,9 @@ function Home(props){
             src="./images/Logo/flameingFeather_lg.png"
             alt="Flaming Feather apothics"
           />
-          <h4>Flaming Feather Apothics</h4>
-          <p> Herbs and and natrual remidies for common ailments and strifes</p>
-          <Button href='#artists' variant='dark'>Artisans directory</Button>
+          <h4>News</h4>
+          <p>Hark and listen to the coming events and goings on of Ravenmoore valley. click below for the latest updates!</p>
+          <Button href='#news' variant='dark'>News</Button>
         </section>
 
         <section className="infoPanel-A" id="artisans">
@@ -50,8 +50,8 @@ function Home(props){
             src="./images/Logo/RavenmoreValley_Logo.png"
             alt="Artisans"
           />
-          <h4>Ravenmoore Valley Consigning Artists</h4>
-          <p>Or Take a Tempting peak at the other fine creators of Ravenmoore Valley</p>
+          <h4>Ravenmoore Valley Artisans</h4>
+          <p>Or Take a Tempting peak at the fine creators of Ravenmoore Valley</p>
           <Button href='#artists' variant='dark'>Artisans directory</Button>
         </section>
       </div>
